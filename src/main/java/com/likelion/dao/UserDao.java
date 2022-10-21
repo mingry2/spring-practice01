f846cda2d0d5a652f268dbdc2ee6ef03933730f9
minkyoung kim <minry20415@naver.com>
2022-10-21 13:02:46 +0900

    [add] userDao 예외처리까지

--- src/main/java/com/likelion/dao/UserDao.java
+++ src/main/java/com/likelion/dao/UserDao.java
@@ -17,6 +17,63 @@ public class UserDao {
         this.cm = cm;
     }
 
+    public void deleteAll() {
+
+        Connection c = null;
+        PreparedStatement ps = null;
+
+        try {
+            c = cm.makeConnection();
+            ps = c.prepareStatement("DELETE FROM users");
+            ps.executeUpdate();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if(ps != null)
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            if(c != null)
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                }
+        }
+    }
+
+    public int getCount() {
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+
+        try {
+            c = cm.makeConnection();
+            ps = c.prepareStatement("select count(*) from users");
+            rs = ps.executeQuery();
+            rs.next();
+            return rs.getInt(1);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        } finally {
+            if(rs != null)
+                try {
+                    rs.close();
+                } catch (SQLException e) {
+                }
+            if(ps != null)
+                try {
+                    ps.close();
+                } catch (SQLException e) {
+                }
+            if(c != null)
+                try {
+                    c.close();
+                } catch (SQLException e) {
+                }
+        }
+    }
+
     public void add(User user) {
         Map<String, String> env = System.getenv();
         try {
@@ -70,8 +127,9 @@ public class UserDao {
 
     public static void main(String[] args) {
         UserDao userDao = new UserDao();
-//        userDao.add();
+        userDao.add(new User("2","BBB","2222"));
         User user = userDao.findById("6");
         System.out.println(user.getName());
+        userDao.deleteAll();
     }
 }
--- src/test/java/com/likelion/dao/UserDaoTest.java
+++ src/test/java/com/likelion/dao/UserDaoTest.java
@@ -8,6 +8,8 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import java.sql.SQLException;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
@@ -20,11 +22,30 @@ class UserDaoTest {
     @Test
     void addAndGet() {
         UserDao userDao = context.getBean("awsUserDao", UserDao.class);
-        String id = "29";
-        userDao.add(new User(id, "EternityHwan", "1234"));
+        String id = "1";
+        userDao.add(new User(id, "AAA", "1111"));
         User user = userDao.findById(id);
 
-        assertEquals("EternityHwan", user.getName());
-        assertEquals("1234", user.getPassword());
+        assertEquals("AAA", user.getName());
+        assertEquals("1111", user.getPassword());
+    }
+
+    @Test
+    void count() {
+
+        User user1 = new User("3","CCC","3333");
+        User user2 = new User("4","DDD","4444");
+        User user3 = new User("5","EEE","5555");
+
+        UserDao userDao = context.getBean("awsUserDao", UserDao.class);
+        userDao.deleteAll();
+        assertEquals(0, userDao.getCount());
+
+        userDao.add(user1);
+        assertEquals(1, userDao.getCount());
+        userDao.add(user2);
+        assertEquals(2, userDao.getCount());
+        userDao.add(user3);
+        assertEquals(3, userDao.getCount());
     }
 }
